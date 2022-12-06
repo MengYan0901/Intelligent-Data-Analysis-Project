cf15d466988e512378e6211f57ce08959eac52c8
MengYan <2285870438@qq.com>
2022-12-06 23:59:51 +0100

    first commit

---
+++ src/plot_confusion_matrix.sh
@@ -0,0 +1,3 @@
+python ./test_raf-db.py \
+    --checkpoint "./models/RAF-DB/epoch26_acc0.9113.pth"  \
+    --plot_cm
---
+++ src/test_raf-db.sh
@@ -0,0 +1,2 @@
+python ./test_raf-db.py \
+    --checkpoint="./models/RAF-DB/epoch26_acc0.9113.pth" 
